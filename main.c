42e073eb61d88b6795c8fe422808b8313a9245d4
OleileiA <972260032@qq.com>
2023-05-15 00:21:20 +0800

    gcc base

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/*
+ * gcc预编译：      gcc -E main.c -o main.i
+ * gcc预编译到汇编： gcc -S main.i -o main.s
+ * gcc汇编到二进制： gcc -c main.s -o main.o
+ * 目标文件：        gcc main.o -o main
+ *
+ * 直接编译：gcc main.c -o main
+ *
+ * 编译两个文件：gcc main.c test.c -o main
+ * */
 int main() {
     printf("Hello, World!\n");
     return 0;
