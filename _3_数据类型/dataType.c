70982c09dcfc7633ba68092d8b8a376ba1649dda
OleileiA <972260032@qq.com>
2023-05-16 00:36:16 +0800

    数据类型

---
+++ _3_数据类型/dataType.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    printf("hello world");
+
+    /*
+     * 整数类型：
+     * int 占用4个字节，表示范围正21亿到负21亿
+     * long 8个字节
+     * short 2个字节
+     */
+
+    /*
+     * 浮点类型：
+     * float 单精度浮点数，4个字节
+     * double 双精度浮点数，8个字节
+     */
+
+    /*
+     * 字符型
+     * char 字符型 -128 ~ 127的表示范围，可以表示全部ASCII码表
+     */
+    return 0;
+}
